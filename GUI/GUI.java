5b2dc81bfb0bb5cf63db9c529865a568ce6113e1
borgarlie <borgar.lie@gmail.com>
2014-02-27 13:07:32 +0100

    Delete all

--- GUI/GUI.java
+++
@@ -1,5 +0,0 @@
-class GUI {
-	public static void main(String[] args) {
-		System.out.println("DETTE SKAL VI KODE!");
-	}
-}
--- TestKlient.java
+++
@@ -1,6 +0,0 @@
-class TestKlient {
-	public static void main(String[] args) {
-		System.out.println("Yo børjis!");
-
-	}
-}
