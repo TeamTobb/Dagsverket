00be369027610a0ee7a6eddd3a48d97e8dc05185
borgarlie <borgar.lie@gmail.com>
2014-02-27 15:36:26 +0100

    Operator added

---
+++ Dagsverket/src/Backend/Operator.java
@@ -0,0 +1,35 @@
+package Backend;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author borgarlie
+ */
+
+public class Operator {
+    private String navn;
+
+    public Operator(String navn) {
+            this.navn = navn;
+    }
+
+    public boolean createEvent(String contractor, String[] employees, ) {
+    	// Check and verify every parameter
+    	if(contractor != null) {
+
+    	}
+
+    	// updateDatabase();
+
+    	return true;
+
+    }
+
+    // kalender osv i annen klasse.
+	
+}
