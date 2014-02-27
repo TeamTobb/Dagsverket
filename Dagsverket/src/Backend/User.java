3c29668836f74748a55f5dea23f79a46704e8c03
borgarlie <borgar.lie@gmail.com>
2014-02-27 14:46:12 +0100

    User class

---
+++ Dagsverket/src/Backend/User.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Backend;
+
+/**
+ *
+ * @author borgarlie
+ */
+
+import java.util.*;
+
+public class User {
+    private ArrayList<String> usernames;
+
+    public User() {
+        this.usernames = getUsernames();
+    }
+
+    private ArrayList<String> getUsernames() {
+    	ArrayList<String> temp = new ArrayList<String>();
+    	return temp;
+    }
+}
