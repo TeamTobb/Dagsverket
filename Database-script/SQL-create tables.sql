bcc31607cb57881b27d532a2df006ea281e63edf
Wimsen <Jorgen@trl185127.ka.hist.no>
2014-02-28 17:00:10 +0100

    Lagt til database script.

---
+++ Database-script/SQL-create tables.sql
@@ -0,0 +1,36 @@
+DROP TABLE events_has_employees;
+DROP TABLE users;
+DROP TABLE events;
+
+CREATE TABLE users(
+id INTEGER GENERATED ALWAYS AS IDENTITY,
+username VARCHAR(30),
+CONSTRAINT users_pk PRIMARY KEY(id)
+);
+
+CREATE TABLE events(
+event_id INTEGER GENERATED ALWAYS AS IDENTITY, 
+contractor VARCHAR(30), 
+phone INTEGER, 
+mail VARCHAR(30),
+address VARCHAR(30), 
+postnr INTEGER, 
+postplace VARCHAR(30), 
+responsible VARCHAR(30), 
+checkup_date VARCHAR(30), 
+tid VARCHAR(30), 
+subject VARCHAR(30),
+description VARCHAR(30), 
+status VARCHAR(30),
+CONSTRAINT events_pk PRIMARY KEY(event_id)
+);
+
+CREATE TABLE events_has_employees(
+event_id INTEGER, 
+user_id INTEGER,
+CONSTRAINT events_has_employees_pk PRIMARY KEY(event_id, user_id)
+);
+
+ALTER TABLE events_has_employees ADD CONSTRAINT event_id_fk FOREIGN KEY(event_id) REFERENCES events(event_id);
+
+ALTER TABLE events_has_employees ADD CONSTRAINT user_id_fk FOREIGN KEY(user_id) REFERENCES users(id);
