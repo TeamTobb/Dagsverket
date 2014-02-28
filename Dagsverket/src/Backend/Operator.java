c5eca07a824e94a004fbf3970abe1a68c753a4de
borgarlie <borgar.lie@gmail.com>
2014-02-28 13:35:02 +0100

    Operator createvent proto

--- Dagsverket/src/Backend/Operator.java
+++ Dagsverket/src/Backend/Operator.java
@@ -1,4 +1,6 @@
-package Backend;
+// package Backend;
+
+// bugg in java?
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -11,7 +13,15 @@ package Backend;
  * @author borgarlie
  */
 
+import java.util.*;
+
 public class Operator {
+    // ERROR MESSAGES
+    public static int NO_CONTRACTOR = 1;
+    public static int NO_CONTACT_INFO = 2;
+    public static int NO_SUBJECT = 3;
+
+    // Object variables
     private String name;
 
     public Operator(String name) {
@@ -22,18 +32,40 @@ public class Operator {
     	return this.name;
     }
 
-    public boolean createEvent(String contractor, String[] employees) {
+    public ArrayList<Integer> createEvent(String contractor, String[] employees, int phone, String mail, String address, int postnr, String postplace, String responsible, String date, String subject, String text, String status) {
     	// Check and verify every parameter
-    	if(contractor != null) {
-    		return true;
+        // add errors to arraylist
+        ArrayList<Integer> errors = new ArrayList<Integer>();
+
+        // important: contractor, phone / mail, subject
+
+    	if(contractor == null || contractor.trim().equals("")) {
+    		errors.add(NO_CONTRACTOR);
     	}
+        if(mail == null && phone <= 0) {
+            errors.add(NO_CONTACT_INFO);
+        }
+        if(subject == null || subject.trim().equals("")) {
+            errors.add(NO_SUBJECT);
+        }
 
-    	// updateDatabase();
+        if(errors == null || errors.size() == 0) {
+            // updateDatabase();
+            return null;
+            // if null on client, move on to next.
+        }
 
-    	return true;
-
+    	return errors;
+        // if errors are returned, handle them on client.
     }
 
     // kalender osv i annen klasse.
+
+    public static void main(String[] args) {
+        // System.out.println("test");
+       Operator op = new Operator("Gunnar");
+       System.out.println(op.createEvent("test123", null, 123, null, null, 444, null, null, null, "hai", null, null));
+       // test for create event
+    }
 	
 }
